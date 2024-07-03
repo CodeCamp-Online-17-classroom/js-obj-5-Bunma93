26e5aa4d446c3a329119e4dd093e890cfaf19054
Bunma93 <bunma93@gmail.com>
2024-07-03 10:43:42 +0700

    Js-obj5

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // write code here
+const product1 = {
+    name: 'Water',
+    distributor: {
+      name: 'Giraffe Water Company',
+      address: {
+        street: 'Phetchaburi',
+        subdistrict: 'Thanonphetchaburi',
+        district: 'Ratchathewi',
+        province: 'Bangkok'
+      }
+    }
+  };
+  console.log(product1.distributor.address.province);
+  let product2 = {};
+  product2 = product1;
+  console.log(product2.distributor.address.province);
